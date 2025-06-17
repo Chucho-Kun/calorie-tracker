7b45e889a5766187a83e3c014ec4ebfa7ba2cea8
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-17 10:23:46 -0600

    implementando contextAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useMemo, useReducer } from "react"
+import { useEffect, useMemo } from "react"
 import Form from "./components/Form"
-import { activityReducer, initialState } from './reducers/activity-reducer';
 import ActivityList from "./components/ActivityList";
 import CalorieTracker from "./components/CalorieTracker";
+import { useActivity } from "./hooks/useActivity";
 
 function App() {
 
-  const [ state , dispatch ] = useReducer( activityReducer , initialState )
+  //const [ state , dispatch ] = useReducer( activityReducer , initialState )
+  const { state , dispatch } = useActivity()
 
   useEffect( () => { 
     localStorage.setItem( 'activities' , JSON.stringify( state.activities ) )
@@ -32,10 +33,7 @@ function App() {
 
       <section className="bg-cyan-600 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form 
-            dispatch={dispatch}
-            state={ state }
-          />
+          <Form />
         </div>
       </section>
         
@@ -43,18 +41,13 @@ function App() {
         className="bg-gray-800 py-10"
       >
         <div className="max-w-4xl mx-auto">
-            <CalorieTracker 
-              activities={ state.activities }
-            />
+            <CalorieTracker />
         </div>
 
       </section>
 
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList 
-          activities={state.activities}
-          dispatch={dispatch}
-        />
+        <ActivityList />
       </section>
     </>
   )
